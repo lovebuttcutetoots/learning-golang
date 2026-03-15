905496230fbc9cbea18f792a8b26a998474e602a
lovebuttcutetoots <brad.clasen@gmail.com>
2026-03-15 00:03:35 -0700

    Exercise 002 - Variables and basic types

---
+++ exercises/002-variables/README.md
@@ -0,0 +1,23 @@
+# Exercise 002 — Variables and Basic Types
+
+This exercise introduces Go variables and the most common built-in data types.
+
+## Concepts Covered
+
+- Variable declaration using `var`
+- Short variable declaration using `:=`
+- Basic Go types
+- Printing variables
+- String formatting with `fmt.Printf`
+
+## Go Types Introduced
+
+| Type | Description |
+|-----|-----|
+| string | Text values |
+| int | Whole numbers |
+| float64 | Decimal numbers |
+| bool | True / false values |
+
+
+
---
+++ exercises/002-variables/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Full Declaration Syntax
+	/*
+		var name string = "Brad"
+		var age int = 42
+		var height float64 = 5.10
+		var learningGo bool = true
+	*/
+
+	//Short Declaration Syntax
+	name := "Brad"
+	age := 42
+	height := 5.10
+	learningGo := true
+
+	// Println Syntax
+	/*
+		fmt.Println("Name:", name)
+		fmt.Println("Age:", age)
+		fmt.Println("Height:", height)
+		fmt.Println("Learning Go:", learningGo)
+	*/
+
+	// Printf Syntax
+	fmt.Printf("Hello, my name is %s.\n", name)
+	fmt.Printf("I am %d years old.\n", age)
+	fmt.Printf("My height is %.2f feet.\n", height)
+	fmt.Printf("Learning Go: %t\n", learningGo)
+
+}
