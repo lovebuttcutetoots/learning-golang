3a287bc460ae978b42f98c1e7442a0dc90f954b0
lovebuttcutetoots <brad.clasen@gmail.com>
2026-03-21 23:31:16 -0700

    Exercise 007 - Structs

---
+++ exercises/007-structs/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name       string
+	Age        int
+	City       string
+	Occupation string
+}
+
+func printPerson(p Person) {
+	fmt.Printf("%s is %d year old, lives in %sm, and works as a %s.\n", p.Name, p.Age, p.City, p.Occupation)
+}
+
+func main() {
+
+	brad := Person{
+		Name:       "Brad",
+		Age:        42,
+		City:       "Las Vegas",
+		Occupation: "loser",
+	}
+
+	alice := Person{
+		Name:       "Alice",
+		Age:        30,
+		City:       "Seattle",
+		Occupation: "winner",
+	}
+
+	sam := Person{
+		Name:       "Sam",
+		Age:        27,
+		City:       "Denver",
+		Occupation: "winner",
+	}
+
+	jordan := Person{
+		Name:       "Jordan",
+		Age:        35,
+		City:       "Phoenix",
+		Occupation: "chicken dinner",
+	}
+
+	people := []Person{brad, alice, sam, jordan}
+
+	for _, person := range people {
+		printPerson(person)
+	}
+}
